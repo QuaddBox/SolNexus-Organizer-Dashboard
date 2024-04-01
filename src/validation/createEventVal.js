a32bb72184aba7a589ff588859d1adaa48cd7e1e
Wutche <wuthchechikaome75@gmail.com>
2024-03-31 21:56:36 -0700

    adding some fields to the ui

--- src/validation/createEventVal.js
+++ src/validation/createEventVal.js
@@ -1,10 +1,12 @@
-import z from 'zod'
+/** @format */
+
+import z from "zod";
 
 export const createEventVal = z.object({
-    eventTitle: z.string({required_error: "Title is required"}),
-    organizer: z.string({required_error: "Organizer is required"}),
-    category: z.string({required_error: "Category is required"}),
-    eventStarts: z.coerce.date(),
-    eventEnds: z.coerce.date()
-    
-})
+	eventTitle: z.string({ required_error: "Title is required" }),
+	organizer: z.string({ required_error: "Organizer is required" }),
+	category: z.string({ required_error: "Category is required" }),
+	description: z.string({ required_error: "Description is required" }),
+	eventStarts: z.coerce.date(),
+	eventEnds: z.coerce.date(),
+});
